a1880faf2e29d5699103fb185a68b82e8ad61ed0
MacStudio <macstudio@example.com>
2025-03-06 19:16:54 +0300

    new version add

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,6 +4,19 @@ import 'screens/home_screen.dart';
 import 'utils/log_setup.dart';
 
 void main() {
+  Log.debug("Initializing app...");
+  Log.info("User logged in successfully.");
+  Log.warning("Low memory detected!");
+  Log.error("Failed to fetch API data!");
+  Log.fatal("System crashed!");
+  Log.json('{ "status": "success", "user": "John Doe" }');
+
+  Log.red("This is a red message.");
+  Log.green("This is a green message.");
+  Log.yellow("This is a yellow message.");
+  Log.blue("This is a blue message.");
+  Log.cyan("This is a cyan message.");
+  Log.magenta("This is a magenta message.");
   setupLogging(); // Configuring log settings before app starts
   Log.info("Application is launching...", fileName: "main.dart");
   runApp(const MyApp());
