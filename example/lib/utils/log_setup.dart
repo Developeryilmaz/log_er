84c9c969d19395bac823e2e84f022962f08a4dd6
MacStudio <macstudio@example.com>
2025-03-06 19:57:48 +0300

    tag

--- example/lib/utils/log_setup.dart
+++
@@ -1,8 +0,0 @@
-import 'package:log_er/log_er.dart';
-
-/// **Configures logging before the app starts**
-void setupLogging() {
-  Log.info("Initializing log settings...");
-
-  // Additional setup (e.g., redirect logs to a file)
-}
